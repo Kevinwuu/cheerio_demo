37e859b26a394b49702a9e4c4fee8b3e340ff5ea
Ethan Wu <405417048@gms.tku.edu.tw>
2024-05-17 17:52:14 +0800

    format code and fix index issue

--- crawData.js
+++ crawData.js
@@ -1,24 +1,28 @@
-const request = require('request');
-const cheerio = require('cheerio');
+const request = require("request");
+const cheerio = require("cheerio");
 
-const url = 'https://www.vscinemas.com.tw/vsweb/film/hot.aspx';
+const url = "https://www.vscinemas.com.tw/vsweb/film/hot.aspx";
+
 const getHotMovieInfo = function () {
-	request({
-		url,
-		method: "GET"
-	},
-		function (error, response, body) {
-			if (error || !body) {
-				return;
-			}
-			const $ = cheerio.load(body);
-			let hottest = $(".info > h1 > a").text();
-			let hotList = $(".infoArea > h2 > a");
-			console.log('1:', hottest);
+  request(
+    {
+      url,
+      method: "GET",
+    },
+    function (error, response, body) {
+      if (error || !body) {
+        return;
+      }
+      const $ = cheerio.load(body);
+      let hottest = $(".info > h1 > a").text();
+      let hotList = $(".infoArea > h2 > a");
+      console.log("1:", hottest);
 
-			hotList.each((i, elem) => {
-				console.log(i + 1 + ':', elem.children[0].data);
-			})
-		});
+      hotList.each((i, elem) => {
+        console.log(i + 2 + ":", elem.children[0].data);
+      });
+    }
+  );
 };
+
 getHotMovieInfo();
